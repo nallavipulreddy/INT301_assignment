f9c652ea7aee903784fd67170217073954a258ee
vipul reddy <nallavipulreddy00@gmail.com>
2020-04-01 23:06:09 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -68,7 +68,7 @@ if (isset($_POST['submit'])) {
 <style type="text/css">
     body{
         color: #fff;
-        background-image: url("https://images.unsplash.com/photo-1531747118685-ca8fa6e08806?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
+        background-image: url("https://images3.alphacoders.com/427/thumb-1920-42785.jpg");
         position: relative;
         background-attachment: fixed;
         background-position: center;
